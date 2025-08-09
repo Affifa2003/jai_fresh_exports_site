802d25a975a7b0d899512e6c4776c8639f36e5fa
Affifa2003 <146504824+Affifa2003@users.noreply.github.com>
2025-08-09 12:20:41 +0530

    mobile responsive update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,8 @@
-// === header.component.ts ===
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-header',
@@ -10,4 +11,17 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  closeMenu(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const navMenu = document.getElementById('navMenu');
+      if (navMenu) {
+        const bsCollapse = bootstrap.Collapse.getInstance(navMenu) || new bootstrap.Collapse(navMenu);
+        bsCollapse.hide();
+      }
+    }
+  }
+}
